4af0963fac8a5f9b65f5cab173798e1f1bf0f072
Michael Babker <michael.babker@gmail.com>
2024-05-07 21:33:07 -0400

    Bump dev deps

--- rector.php
+++ rector.php
@@ -2,39 +2,34 @@
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/config',
         __DIR__.'/src',
         __DIR__.'/tests',
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         /*
          * Skip selected rules
          */
         AddSeeTestAnnotationRector::class,
-    ]);
-
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-    $rectorConfig->phpstanConfigs([
+    ])
+    ->withImportNames(importShortClasses: false)
+    ->withPHPStanConfigs([
         __DIR__ . '/vendor/phpstan/phpstan-phpunit/extension.neon',
         __DIR__ . '/vendor/phpstan/phpstan-symfony/extension.neon',
         __DIR__ . '/phpstan.neon',
-    ]);
-
-    $rectorConfig->sets([
+    ])
+    ->withSets([
         LevelSetList::UP_TO_PHP_82,
         SetList::CODE_QUALITY,
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_90,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        SymfonyLevelSetList::UP_TO_SYMFONY_63,
-    ]);
-};
+        SymfonySetList::SYMFONY_64,
+    ])
+    ->withPreparedSets(codeQuality: true)
+;
